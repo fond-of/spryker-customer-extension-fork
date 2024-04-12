3e3f060e8da6df27f5ed34a3c8666f725219c933
Julian Hyatt <dev@jzit.de>
2024-04-12 10:34:38 +0200

    provides new interfaces for - post update customer - pre update customer - post add customer

---
+++ src/Spryker/Zed/CustomerExtension/Dependency/Plugin/CustomerPreAddPluginInterface.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Zed\CustomerExtension\Dependency\Plugin;
+
+use Generated\Shared\Transfer\CustomerTransfer;
+
+/**
+ * Provides an ability to execute additional actions before a customer is added.
+ */
+interface CustomerPreAddPluginInterface
+{
+    /**
+     * Specification:
+     * - Executes additional actions before a customer is added.
+     *
+     * @api
+     *
+     * @param \Generated\Shared\Transfer\CustomerTransfer $customerTransfer
+     *
+     * @return void
+     */
+    public function execute(CustomerTransfer $customerTransfer);
+}
